57d3c42870feb3f5d7326562f0a3a85a6a4619cd
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-11-29 09:11:27 -0800

    adding components for login and modal

--- client/src/pages/signup/signup.js
+++ client/src/pages/signup/signup.js
@@ -3,27 +3,40 @@ import React, { Component } from "react";
 const Signup = () => {
   render() {
     return (
-      <form action="/api/signup" method="POST">
-         <div className="form-group">
-            <label>Email</label>
-            <input type="text" className="form-control" name="email" />
-        </div>
-        <div className="form-group">
-            <label>Create Username</label>
-            <input type="text" className="form-control" name="username" />
-        </div>
-        <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" name="password" />
-        </div>
-        <div className="form-group">
-            <label>Confirm Password</label>
-            <input type="password" className="form-control" name="passwordConfirm" />
-        </div>
+      <div>
+        <form action="/api/signup" method="POST">
+           <div className="form-group">
+              <label>First Name</label>
+              <input type="text" className="form-control" name="firstName" />
+          </div>
+          <div className="form-group">
+              <label>Last Name</label>
+              <input type="text" className="form-control" name="lastName" />
+          </div>
+           <div className="form-group">
+              <label>Email</label>
+              <input type="text" className="form-control" name="email" />
+          </div>
+          <div className="form-group">
+              <label>Create Username</label>
+              <input type="text" className="form-control" name="userName" />
+          </div>
+          <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" name="passWord" />
+          </div>
+          <div className="form-group">
+              <label>Confirm Password</label>
+              <input type="password" className="form-control" name="passwordConfirm" />
+          </div>
 
-        <button type="submit" className="btn btn-warning btn-lg">Signup</button>
-      </form>
+          <button type="submit" className="btn btn-warning btn-lg">Signup</button>
+        </form>
 
+        <hr/>
+        <p>Already have an account? <a href="/login">Login</a></p>
+        <p>Or go <a href="/">home</a>.</p>
+    </div>
       )
   }
 
