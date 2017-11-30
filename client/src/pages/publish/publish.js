e683da57e381104a5969e35d043a9e78477d2194
david morse <davidbryanmarshall@gmail.com>
2017-11-29 23:04:04 -0800

    basic publish page and cloudinary keys config

---
+++ client/src/pages/publish/publish.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Publish extends Component{
+  render() {
+    return (
+      <div className="container">
+        Publish
+    </div>
+      )
+  }
+
+}
+
+export default Publish;
