90fc7a0b937ec847331b18ffd566f97d94a018a2
david morse <davidbryanmarshall@gmail.com>
2017-11-30 03:55:20 -0800

    removing jquery and bootstrap js calls

--- client/src/pages/publish/publish.js
+++ client/src/pages/publish/publish.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import {Image} from 'cloudinary-react';
+// Or for more advanced usage:
+// import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
 
 class Publish extends Component{
-  render() {
-    return (
-      <div className="container">
-        Publish
-    </div>
-      )
-  }
+  
+    render(){
+        return (
+            <div className="main">
+                <h1>Publish</h1>
+
+                <Image cloudName="cali-cool" publicId="fsreqp3pxpjxku5trmxb" width="200" height="200" crop="thumb"/>
+                <br/>
+                thumbnail cropped at 200x200
+                
+            </div>
+
+        );
+    }
 
 }
 
