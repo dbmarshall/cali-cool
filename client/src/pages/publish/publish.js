4434ab88f1597c8dd84bedc483eaf0ee45a0ce35
david morse <davidbryanmarshall@gmail.com>
2017-12-01 02:27:26 -0800

    basic file upload layout and working preview image

--- client/src/pages/publish/publish.js
+++ client/src/pages/publish/publish.js
@@ -4,38 +4,36 @@ class Publish extends Component {
 
   state = {
     results: [],
+    file: '', 
+    name: '', 
+    imagePreviewUrl: ''
   };
 
   componentDidMount() {
-    this.loadArticles()
+    // this.loadArticles()
   }
 
-  openModal = (commentArticleId, commentArticleTitle)  => this.setState({ 
-    open: true, 
-    comment: "", 
-    commentArticleId: commentArticleId, 
-    commentArticleTitle: commentArticleTitle
-  });
+  handleBrowse = event => {
 
-  closeModal = () => this.setState({
-    open: false, 
-    comment: ""
-  });
- 
-  saveComment = () => {
-  };
+    let reader = new FileReader();
+    let file = event.target.files[0];
+    let name = event.target.files[0].name;
 
-  loadArticles = () => {
-  };
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        name: name,
+        imagePreviewUrl: reader.result
+      });
 
-  saveArticle = targetIndex => {
-  };
+      // console.log('file: ', this.state.file)
+      // console.log('name: ', this.state.name)
+      // console.log('imagePreviewUrl: ', this.state.imagePreviewUrl)
+    };
 
-  deleteArticle = id => {
-  };
+    reader.readAsDataURL(file);
 
-  deleteComment = id => {
-  };
+  }
 
   handleInputChange = event => {
     const { name, value } = event.target;
@@ -45,6 +43,12 @@ class Publish extends Component {
   };
 
   handleFormSubmit = event => {
+    event.preventDefault();
+    // API.apiPublish(this.state.topic, this.state.startyear, this.state.endyear)
+    //   .then(res => {
+    //     this.setState({ results: res, topic: "", startyear: "", endyear: "" });
+    //   })
+    //   .catch(err => console.log(err));
   };
 
   render() {
@@ -59,7 +63,81 @@ class Publish extends Component {
                   <h1>Publish</h1>
                 </div>
                 <div className="panel-body">
-                  page content 
+
+                  <div className="row">
+                    <div className="col-md-5 col-md-offset-1">
+                      <div className="panel panel-default">
+                        <div className="panel-body">
+                    
+                          <form onSubmit={this.handleFormSubmit}>
+                            {/*<div className="form-group">
+                              <label htmlFor="topic">Topic:</label><br/>
+                              <input
+                                value={this.state.topic}
+                                onChange={this.handleInputChange}
+                                name="topic"
+                                placeholder="Enter Search Topic"
+                                className="form-control" 
+                                id="topic"
+                              />
+                            </div>*/}
+                            <div className="form-group">
+                              <label htmlFor="fileupload">Choose an image to upload:</label><br/>
+                              <div className="input-group">
+                                <label className="input-group-btn">
+                                    <span className="btn btn-primary">
+                                        Browse&hellip; 
+                                        <input 
+                                          onChange={this.handleBrowse}
+                                          type="file" 
+                                          name="fileupload" 
+                                          style={{display: 'none'}} 
+                                          multiple 
+                                         />
+                                    </span>
+                                </label>
+                                <input 
+                                  value={this.state.name}
+                                  type="text" 
+                                  className="form-control"  
+                                  readOnly
+                                />
+                              </div>
+                              <button 
+                                type="submit" 
+                                className="btn btn-default btn-primary" 
+                                style={{marginTop: '10px'}}>
+                                Upload
+                              </button>
+                            </div>
+                          </form>
+
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-md-5">
+                      <div className="panel panel-default">
+                        <div className="panel-body">
+                    
+                          <div id="img-preview">
+                            {/*{imagePreview}*/}
+                            <img 
+                              src={this.state.imagePreviewUrl} 
+                              id="img-preview" 
+                              alt="Upload preview..." 
+                              style={{
+                                maxWidth: '100%', 
+                                maxHeight: '118px'
+                              }}
+                            />
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
             </div>
