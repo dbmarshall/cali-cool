901e6571508d750ed7b9a77f376d0a59f7323425
david morse <davidbryanmarshall@gmail.com>
2017-11-30 22:31:59 -0800

    removing cloudinary-react package

--- client/src/pages/publish/publish.js
+++ client/src/pages/publish/publish.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-// import cloudinary from 'cloudinary';
-import {Image} from 'cloudinary-react';
-// Or for more advanced usage:
-// import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
 
 class Publish extends Component{
   
@@ -11,9 +7,7 @@ class Publish extends Component{
             <div className="main">
                 <h1>Publish</h1>
 
-                <Image cloudName="cali-cool" publicId="fsreqp3pxpjxku5trmxb" width="200" height="200" crop="thumb"/>
-                <br/>
-                thumbnail cropped at 200x200
+                
             </div>
 
         );
