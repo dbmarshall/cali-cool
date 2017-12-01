92d06bdb9322d498e4402c8198405c95be828d04
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-12-01 00:22:29 -0800

    adding modal into header component and removing packages

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,37 +1,88 @@
-import React from "react";
-import LoginModal from "../LoginModal";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import {Modal} from 'react-bootstrap';
 
-const Header = () =>
- <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to="/">Cali.Cool</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} >
-          <Link to="/user/:id">My Account</Link>
-        </NavItem>
-        <NavItem eventKey={2} >
-           <Link to="/publish">Publish</Link>
-        </NavItem>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} > 
-          <Link to="/signup">Signup</Link>
-        </NavItem>
-        <NavItem eventKey={2} >
-          <Link to="/login">Login</Link>
-        </NavItem>
-        <NavItem eventKey={3}> 
-          <Link to="/logout">Logout</Link>
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+class Header extends Component {
+
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+    open: false
+    }
+  };
+
+  showModal = event => {
+    this.setState({open: true})}
+
+  closeModal = event => {
+      this.setState({ open: false })}
+ 
+  saveAndClose = event => {
+    this.setState({ open: false })};
+
+  render(){
+
+    return (
+       <div>
+     <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Cali.Cool</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} >
+              <Link to="/user/:id">My Account</Link>
+            </NavItem>
+            <NavItem eventKey={2} >
+               <Link to="/publish">Publish</Link>
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} > 
+              <Link to="/signup">Signup</Link>
+            </NavItem>
+            <NavItem eventKey={2} >
+              <p
+              onClick={this.showModal} >Login
+              </p>
+            </NavItem>
+            <NavItem eventKey={3}> 
+              <Link to="/logout">Logout</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+        
+      <Modal
+        show={this.state.open}
+        onHide={this.closeModal}
+        aria-labelledby="ModalHeader"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id='ModalHeader'>A Title Goes here</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Some Content here</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
+          <button 
+          className='btn btn-primary' 
+          onClick={this.saveAndClose}>
+            Save
+          </button>
+        </Modal.Footer>
+      </Modal>
+  </div>
+      )
+  }
+
+
+}
 
 export default Header;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = function(router, passport) {
   //   res.send("route hit today")
   // });
 
-  router.post('/api/signup', passport.authenticate('local-signup', {
+  router.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
