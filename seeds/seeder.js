8cfe6b06b83362c453ff91e46c2226d815ac01a7
david morse <davidbryanmarshall@gmail.com>
2017-12-04 10:57:00 -0800

    updating seeder file to run on heroku

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -2,18 +2,18 @@ var seeder = require('mongoose-seed');
 const mongoose = require("mongoose");
  
 // Connect to MongoDB via Mongoose
-seeder.connect('mongodb://localhost/calicool_db', function() {
- 
+seeder.connect(process.env.MONGODB_URI || "mongodb://localhost/calicool_db", function() {
+
   // Load Mongoose models
   seeder.loadModels([
-    '../models/Users.js',
-    '../models/Invitees.js',
-    '../models/Tags.js',
-    '../models/Albums.js',
-    '../models/Photos.js',
-    '../models/Comments.js',
-    '../models/PhotoTags.js',
-    '../models/AlbumTags.js'
+    './models/Users.js',
+    './models/Invitees.js',
+    './models/Tags.js',
+    './models/Albums.js',
+    './models/Photos.js',
+    './models/Comments.js',
+    './models/PhotoTags.js',
+    './models/AlbumTags.js'
   ]);
  
   // Clear specified collections
