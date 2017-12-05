8d3cd482465f42cd873d9672f8ff3d64cae81f91
Josh Siverson <28927991+siverson90@users.noreply.github.com>
2017-12-05 08:25:34 -0800

    change my name

--- routes/api/photos.js
+++ routes/api/photos.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const photosController = require("../../controllers/photosController");
 
-//Routes goes here — match with "/api/photos"
+//Routes goes here — match with "/api/photos" 
 router.route("/recent")
   .get(photosController.recent);
 
