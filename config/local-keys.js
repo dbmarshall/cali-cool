5fd5bde012af304cb8491d522164a88024cf348c
david morse <davidbryanmarshall@gmail.com>
2017-12-06 15:07:53 -0800

    update local

--- config/local-keys.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = { 
-  
-}
-
-// heroku config:set cloud_name=cali-cool api_key=156471978987996 api_secret=9_kPV20u0vChrWaHtoC0iLtLb4Q secret=california 
-// 
-// git push heroku feature/heroku:master
