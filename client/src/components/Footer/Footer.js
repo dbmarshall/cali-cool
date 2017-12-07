29764d9265da6a3b820d6a6abbe8c070c392f80d
david morse <davidbryanmarshall@gmail.com>
2017-12-06 21:41:33 -0800

    added copyright to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () =>
   <footer className="footer">
-    <span>Cali.Cool 2017</span>
+    <span>&copy; Cali.Cool 2017</span>
   </footer>;
 
 export default Footer;
